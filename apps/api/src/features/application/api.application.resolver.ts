820999f8933711d86caffde0009beaa8cd4349ea
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2023-08-02 21:15:04 +0700

    feat(Application): Người dùng truy vấn application

--- apps/api/src/features/application/api.application.resolver.ts
+++ apps/api/src/features/application/api.application.resolver.ts
@@ -1,7 +1,9 @@
 import { BaseCRUDResolver, CurrentUser, JwtPayload, Resource } from "@app/core";
 import { ApplicationRepository } from "@app/dal";
+import { FindAllArgs } from "@app/shared";
+
 import { CommandBus } from "@nestjs/cqrs";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { ResourceEnum } from "../../shared";
 import { Application, ApplicationEdges, CreateApplicationDto } from "./api.application.dto";
 import { CreateApplicationCommand } from "./commands";
@@ -18,5 +20,25 @@ export class ApiApplicationResolver extends BaseCRUDResolver(Application, {
   @Mutation(() => Application, { name: "createApplication" })
   async create(@Args("input") input: CreateApplicationDto, @CurrentUser() user: JwtPayload) {
     return this.commandBus.execute(CreateApplicationCommand.create({ name: input.name, userId: user.userId }));
+  }
+
+  @Resource(ResourceEnum.USER_OWN)
+  @Query(() => ApplicationEdges, { name: `findAllUserApplication` })
+  async findAllUserApplication(@Args() options: FindAllArgs, @CurrentUser() user: JwtPayload) {
+    const { page = 1, limit = 20, filter, order, search } = options?.query || {};
+    const { data, pagination } = await this.appRepo.findWithPagination({
+      filter: {
+        ...filter,
+        ownerId: user.userId,
+      },
+      page: page,
+      limit: limit,
+      sort: order,
+      search: search,
+    });
+    return {
+      data: data,
+      pagination: pagination,
+    };
   }
 }
